1716ef9b24df085160807d8e66c0e68e3c269a9f
Daniye32 <82909899+Daniye32@users.noreply.github.com>
2021-04-20 15:57:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+basic.pause(200)
+basic.showLeds(`
+    . # # # .
+    . # . . #
+    . # . . #
+    . # . . #
+    . # # # .
+    `)
+music.playMelody("C5 B A G F E D C ", 120)
+basic.pause(200)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+music.playMelody("C D E F G A B C5 ", 120)
+basic.pause(100)
+basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+music.playMelody("E B C5 A B G A F ", 120)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+music.playMelody("A F E F D G E F ", 120)
+basic.pause(100)
+basic.showLeds(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . # . . .
+    . # # # .
+    `)
+music.playMelody("C5 A B G A F G E ", 120)
+basic.pause(100)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # . . .
+    . # . . .
+    . # # # .
+    `)
